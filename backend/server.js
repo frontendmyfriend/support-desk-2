228b42a9f1463fa49782536e6133d86de1d8e508
Mary Ti <4younginiverse@gmail.com>
2026-01-23 14:22:10 +0200

    added .env

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server')
+const express = require('express');
+const dotenv = require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req,res)=>{ 
+    res.status(200).json({message:'Welcome to hell'})
+})
+
+app.listen(PORT, ()=> console.log(`server started on port ${PORT}`))
