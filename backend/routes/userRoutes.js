225290fc435f277c278b4ad26c82033258bfbe46
Mary Ti <4younginiverse@gmail.com>
2026-01-24 14:09:22 +0200

    set user controller, added auth and error middleware, create private routes: register and login done in backend

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const {registerUser, loginUser, getMe} = require('../controllers/userController')
+
+
+
+// router.post('/', (req,res) =>{
+//     res.send('register route')
+// })
+
+const {protect} = require('../middleware/authMiddleware')
+
+
+router.post('/', registerUser)
+
+router.post('/login', loginUser)
+
+router.get('/me', protect, getMe)
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
+const colors = require('colors')
+const {errorHandler} =  require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+const PORT = process.env.PORT || 8000
+
+//connect to db
+connectDB()
 
 const app = express();
-const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 
 app.get('/', (req,res)=>{ 
     res.status(200).json({message:'Welcome to hell'})
 })
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, ()=> console.log(`server started on port ${PORT}`))
